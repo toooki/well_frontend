1a3e11d44a3de9a6798a420235007e9823356719
mark797 <mark0594@naver.com>
2024-09-26 18:11:23 +0900

    마지막 시도

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
 import React, { useEffect } from "react";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import './index.css';
-import { getResizeEventListener } from './service/index';
-import Main from './components/Main';
-import Page from './components/Modules/Pages';
-import formData from './components/FormLogin';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./index.css";
+import { getResizeEventListener } from "./service/index";
+import Main from "./components/Main";
+import Page from "./components/Modules/Pages";
+import formData from "./components/FormLogin";
+import "./css/auth.css";
 
 const App = () => {
-    useEffect(() => {
-        const FixRatio = getResizeEventListener(1024, 1366);
-        window.onresize = FixRatio;
-        FixRatio();
-    
-        return () => {
-          window.onresize = null;
-        };
-      }, []);
-      
-    return (
-        <Routes>
-            <Route path="/" element={<Main />}/>
-            <Route path="main/" element={<Page username={formData.username} password={formData.password}/>} />
-        </Routes>
-    );
+	useEffect(() => {
+		const FixRatio = getResizeEventListener(1024, 1366);
+		window.onresize = FixRatio;
+		FixRatio();
+
+		return () => {
+			window.onresize = null;
+		};
+	}, []);
+
+	return (
+		<Routes>
+			<Route path="/" element={<Main />} />
+			<Route
+				path="main/"
+				element={
+					<Page username={formData.username} password={formData.password} />
+				}
+			/>
+		</Routes>
+	);
 };
 
 const AppWrapper = () => {
-    return (
-    <Router>
-        <App/>
-    </Router>);
+	return (
+		<Router>
+			<App />
+		</Router>
+	);
 };
 
 export default AppWrapper;
@@ -46,10 +53,10 @@ export default AppWrapper;
 //     //    <Main/>
 //     //  </div>
 //     // </Router>
-//     <Router> 
+//     <Router>
 //       <Total/>
 //     </Router>
-    
+
 //   );
 // };
 
