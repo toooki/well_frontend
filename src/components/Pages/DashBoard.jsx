b0c6db4c00c9dd9d6588005432ef936d7c843153
PAFF <mark0594@naver.com>
2024-09-27 14:56:42 +0900

    menu

--- src/components/Pages/DashBoard.jsx
+++ src/components/Pages/DashBoard.jsx
@@ -6,71 +6,70 @@ import Clock from "../Blocks/Clock";
 // context
 import { AuthContext } from "../../context/AuthContext";
 import { PageContext } from "../../context/PageContext";
-// svg
-import { ReactComponent as Left } from "../../assets/icons/Left.svg";
-import { ReactComponent as Right } from "../../assets/icons/Right.svg";
 // css
 import "../../css/Menu/Menu.css";
 
 const DashBoard = (props) => {
-	const menuContainerRef = useRef(null);
-	const { currentPage } = useContext(PageContext);
-	const { userData, profile, fetchProfile } = useContext(AuthContext);
-	const [progressCount, setProgressCount] = useState(0);
+    const { currentPage } = useContext(PageContext);
+    const { userData, profile, fetchProfile } = useContext(AuthContext);
+    const [progressCount, setProgressCount] = useState(0);
 
-	useEffect(() => {
-		if (currentPage === "dashboard") {
-			fetchProfile(userData.username);
-		}
-	}, [currentPage, userData.username]);
+    useEffect(() => {
+        if (currentPage === "dashboard") {
+            fetchProfile(userData.username);
+        }
+    }, [currentPage, userData.username]);
 
-	useEffect(() => {
-		if (currentPage === "dashboard") {
-			setProgressCount(
-				Object.values(profile).filter((value) => value === 1).length
-			);
-		}
-	}, [profile, currentPage]);
+    useEffect(() => {
+        if (currentPage === "dashboard") {
+            setProgressCount(
+                Object.values(profile).filter((value) => value === 1).length
+            );
+        }
+    }, [profile, currentPage]);
 
-	const handleClick = (index) => {
-		props.handleItemClick(index);
-	};
+    const handleClick = (index) => {
+        props.handleItemClick(index);
+    };
 
-	const menuNames = [
-		"건강체크",
-		"사전의료의향서",
-		"자성시간",
-		"유언장",
-		"자원봉사",
-		"버킷리스트",
-		"추억 사진",
-		"마음의 빚 청산",
-		"연락처 정리",
-		"장례계획",
-	];
+    const menuNames = [
+        "건강체크",
+        "사전의료의향서",
+        "자성시간",
+        "유언장",
+        "자원봉사",
+        "버킷리스트",
+        "추억 사진",
+        "마음의 빚 청산",
+        "연락처 정리",
+        "장례계획",
+    ];
 
-	const menuItems = menuNames.map((name, i) => (
-		<div className="menuItem" key={i} onClick={() => handleClick(i + 2)}>
-			<button>{name}</button>
-		</div>
-	));
+    const menuItems = menuNames.map((name, i) => (
+        <div className="menuItem" key={i} onClick={() => handleClick(i + 2)}>
+            <button>{name}</button>
+        </div>
+    ));
 
-	return (
-		<motion.div
-			className="content"
-			initial={{ opacity: 0, y: 60 }}
-			animate={{ opacity: 1, y: 0 }}
-			transition={{ duration: 0.5 }}
-		>
-			<Clock />
-			<div className="dashWrapper">
-				<div className="menuContainer" ref={menuContainerRef}>
-					{menuItems}
-				</div>
-			</div>
-			<ProgressBlock per={progressCount} max={10} />
-		</motion.div>
-	);
+    return (
+        <motion.div
+            className="content"
+            initial={{ opacity: 0, y: 60 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+        >
+            <Clock />
+            <div className="progress-container">
+                <div className="progress-text">
+                    전체 {menuNames.length}개 중 {progressCount}개 완료
+                </div>
+                <ProgressBlock per={progressCount} max={menuNames.length} />
+            </div>
+            <div className="dashWrapper">
+                <div className="menuContainer">{menuItems}</div>
+            </div>
+        </motion.div>
+    );
 };
 
 export default DashBoard;
